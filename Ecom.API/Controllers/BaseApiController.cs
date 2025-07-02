83e3e535164b74934d7da06d1c7fee3cbb6946e3
Ahmed Elnezily <elneziliiyahmed@gmail.com>
2025-07-02 09:21:24 +0300

    Added Categories Controleer

---
+++ Ecom.API/Controllers/BaseApiController.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        protected readonly IUnitOfWork _work;
+
+        public BaseApiController(IUnitOfWork work)
+        {
+            _work = work;
+        }
+    }
+}
---
+++ Ecom.API/Controllers/CategoriesController.cs
@@ -0,0 +1,111 @@
+﻿using Ecom.Core.DTOs;
+using Ecom.Core.Entities.Product;
+using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.API.Controllers
+{
+
+    public class CategoriesController : BaseApiController
+    {
+        public CategoriesController(IUnitOfWork work) : base(work) { }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                var categories = await _work.CategoryRepository.GetAllAsync();
+                if (categories == null || !categories.Any())
+                {
+                    return BadRequest("No categories found.");
+                }
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            try
+            {
+                var category = await _work.CategoryRepository.GetByIdAsync(id);
+                if (category == null)
+                {
+                    return BadRequest($"Category with ID {id} not found.");
+                }
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPost("add-category")]
+        public async Task<IActionResult> add(CategoryDTO categoryDTO)
+        {
+            try
+            {
+
+                var category = new Category
+                {
+                    Name = categoryDTO.Name,
+                    Description = categoryDTO.Description
+                };
+                await _work.CategoryRepository.AddAsync(category);
+                return Ok(new { message = "New Category Added" });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+
+        }
+        [HttpPut("update-category")]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDTO categoryDTO)
+        {
+            try
+            {
+                var category = await _work.CategoryRepository.GetByIdAsync(categoryDTO.id);
+                if (category == null)
+                {
+                    return NotFound($"Category with ID {categoryDTO.id} not found.");
+                }
+                category.Name = categoryDTO.Name;
+                category.Description = categoryDTO.Description;
+
+                await _work.CategoryRepository.UpdateAsync(category);
+                return Ok(new { message = "Category Updated" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpDelete("delete-category/{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            try
+            {
+                var category = await _work.CategoryRepository.GetByIdAsync(id);
+                if (category == null)
+                {
+                    return NotFound($"Category with ID {id} not found.");
+                }
+                await _work.CategoryRepository.DeleteAsync(id);
+                return Ok(new { message = "Category Deleted" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
